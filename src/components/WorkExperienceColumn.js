774956dee97842297ebec27eb18996b8a9549878
Kyle Ross <kylejamesross@gmail.com>
2023-09-18 23:21:44 -0600

    Remove portfolio page

--- src/components/WorkExperienceColumn.js
+++ src/components/WorkExperienceColumn.js
@@ -2,11 +2,9 @@ import React from "react"
 import WorkExperienceSection from "./sections/work-experience/WorkExperienceSection"
 import ReferencesSection from "./sections/references/ReferencesSection"
 import RightLayoutGridColumn from "./RightLayoutGridColumn"
-import SiteLink from "./SiteLink"
 
 const WorkExperienceColumn = () => (
   <RightLayoutGridColumn>
-    <SiteLink to="/portfolio">Portfolio</SiteLink>
     <WorkExperienceSection />
     <ReferencesSection />
   </RightLayoutGridColumn>
