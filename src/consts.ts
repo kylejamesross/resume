9f4027f121fcbb5324f8d712d94a58db68a13d4c
Kyle Ross <github@kylejamesross.com>
2024-09-15 22:31:19 -0600

    Update astro and add skill groups

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,24 @@
 import { jsonResumeSchema } from "./jsonResumeSchema";
 
 export const portfolioJSON = await fetch('https://gitconnected.com/v1/portfolio/kylejamesross').then((response) => response.json()).then((json) => jsonResumeSchema.parse(json));
+
+type GroupByCallback<T> = (element: T, index: number) => string | symbol;
+
+const groupBy = function<T>(items: Iterable<T>, callbackFn: GroupByCallback<T>): Record<string | symbol, T[]> {
+  const result: Record<string | symbol, T[]> = Object.create(null);
+
+  Array.from(items).forEach((item, index) => {
+    const key = callbackFn(item, index);
+    const groupKey = key.toString(); // Ensure key is coerced to string if necessary
+
+    if (!result[groupKey]) {
+      result[groupKey] = [];
+    }
+
+    result[groupKey].push(item);
+  });
+
+  return result;
+};
+
+export const skillGroups = Object.entries(groupBy(portfolioJSON.skills, (skill) => skill.keywords.at(0)));
